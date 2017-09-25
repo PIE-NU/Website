68ea322e47b479bac6ad85a3bd685c5b802368c0
Murphy Angelo <mca941@pie.tech.northwestern.edu>
2017-09-25 11:10:51 -0500

    Serve public files

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,8 +9,5 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, HOST);
