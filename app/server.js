304d06de9dfc4a7e9ac971a66495e1b6b4fe27c0
Murfalo <murphycangelo@u.northwestern.edu>
2017-09-27 16:09:09 -0500

    Log server requests

--- app/server.js
+++ app/server.js
@@ -9,5 +9,9 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(function(req, res, next) {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, HOST);
