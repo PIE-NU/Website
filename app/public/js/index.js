0da9d2ca195e5f56b32b1c44a6a54944d400b40b
Murfalo <murphycangelo@u.northwestern.edu>
2017-10-01 21:48:56 -0500

    Title screen with sticky navbar

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,3 +1,25 @@
-var s = skrollr.init({
-	// Options
+var flashText = function () {
+	var flashingTexts = document.getElementsByClassName('text-flashing');
+	setInterval(function() {
+		for (var i = 0; i < flashingTexts.length; i++) {
+			flashingTexts[i].style.display = (flashingTexts[i].style.display == 'none' ? '' : 'none');
+		}
+	}, 500);
+}
+
+var stickNavbar = function () {
+	var navbar = document.getElementById('pie-nav');
+
+	if (window.scrollY >= document.getElementById('pie-sky').offsetHeight)
+		navbar.classList.add('sticky');
+	else
+		navbar.classList.remove('sticky');
+}
+
+window.addEventListener('load', function() {
+	flashText();
+}, false);
+
+window.addEventListener('scroll', function() {
+	stickNavbar();
 });
