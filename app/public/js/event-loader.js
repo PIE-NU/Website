4f0a8cb716969431eeb3cc7f8802485b1996a93e
angelomatni <angelomatni2018@u.northwestern.edu>
2017-10-31 00:18:41 -0500

    Setup event list html templating and json loading

---
+++ app/public/js/event-loader.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  // Load event information stored in JSON
+  $.getJSON("json/events.json", function(json) {
+    var eventHtml = ["_event-odd", "_event-even"];
+    var evCount = 0;
+    var $evHolder = $("#event-holder");
+    for (var evName in json) {
+      $evHolder.append("<div id='" + evName + "'></div>");
+      $ev = $("#" + evName);
+      $ev.load("html/" + eventHtml[evCount%2] + ".html", populateEvent($ev,json[evName]));
+      evCount++;
+    }
+  });
+
+  function populateEvent(ev, json) {
+    return function() {
+      for (var evInfo in json) {
+        ev.find("." + evInfo).html(json[evInfo]);
+      }
+    }
+  }
+});
