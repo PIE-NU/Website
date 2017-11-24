3203af1f91ed01a0174245d0b8e40e0aeffc8f15
Murfalo <murphycangelo@u.northwestern.edu>
2017-11-24 18:06:34 -0500

    Minor improvements and removed unused files

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -13,6 +13,7 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 });
 var hasEntered = false;
 var hasLoaded = false;
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 var pieNavbar;
 var pieTitle;
 var pieBody;
@@ -44,6 +45,39 @@ var setBodyMargin = function () {
 	document.scrollTop -= pieNavbar.clientHeight;
 }
 
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+    onKeyDown(e);
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
 // TODO: Cause scrolling to underline an anchor
 var selectAnchor = function (anchor, toggle) {
 	var anchor = document.getElementById(anchor);
@@ -57,6 +91,7 @@ window.addEventListener('load', function() {
 	pieBody = document.getElementById(bodyId);
 	flashText();
 	hasLoaded = true;
+	disableScroll();
 });
 
 window.addEventListener('scroll', function() {
@@ -71,6 +106,10 @@ window.addEventListener('resize', function() {
 
 /* Header Scrolling */
 
+$(window).on('beforeunload', function() {
+	$(window).scrollTop(0);
+});
+
 $(".scroll").on('click', function() {
 	var pieNavHeight = $("#pie-nav").height();
 	var scrollToId = $(this).attr("id").split("-")[1];
@@ -79,7 +118,7 @@ $(".scroll").on('click', function() {
 
 /* Key Handling */
 
-document.onkeydown = function(ev)
+var onKeyDown = function(ev)
 {
 	switch (ev.keyCode)
 	{
@@ -88,6 +127,8 @@ document.onkeydown = function(ev)
 			if (hasEntered) return;
 			scroll.animateScroll(pieNavbar);
 			hasEntered = true;
+			$('#pie-nav ul li').css('opacity', 1);
+			enableScroll();
 			break;
 	}
 }
