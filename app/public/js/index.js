569e7402caa7ba41a12a975a164ef34c4a6db137
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2017-11-27 02:57:18 +0000

    BUGFIX: Image hover borders, scroll and link navigation

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -61,11 +61,13 @@ function preventDefaultForScrollKeys(e) {
 }
 
 function disableScroll() {
+  /*
   if (window.addEventListener) // older FF
       window.addEventListener('DOMMouseScroll', preventDefault, false);
   window.onwheel = preventDefault; // modern standard
   window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
   window.ontouchmove  = preventDefault; // mobile
+  */
   document.onkeydown  = preventDefaultForScrollKeys;
 }
 
